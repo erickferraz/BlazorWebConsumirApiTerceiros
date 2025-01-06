a5441a42ef87d661a1cec6df8d6c317c07a4fdfa
Carlos Erick Ferraz do Prado <erickferraz.prado@gmail.com>
2025-01-06 10:36:45 -0300

    ConsumindoApiTerceirosBlazor

---
+++ Models/Dolar.cs
@@ -0,0 +1,23 @@
+﻿namespace BlazorWebConsumirApiTerceiros.Models;
+
+
+public class Dolar
+{
+    public USDBRL USDBRL { get; set; }
+}
+
+public class USDBRL
+{
+    public string code { get; set; }
+    public string codein { get; set; }
+    public string name { get; set; } // nome moeada 
+    public string high { get; set; } // cotação máxima
+    public string low { get; set; }  // cotação mínima
+    public string varBid { get; set; }
+    public string pctChange { get; set; }
+    public string bid { get; set; }
+    public string ask { get; set; } // cotação atual
+    public string timestamp { get; set; }
+    public string create_date { get; set; } // data cotação
+}
+
